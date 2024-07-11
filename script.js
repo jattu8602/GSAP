6da9ebf9c6bec45440774315a0ca86eed70c27e8
Jatin <chaurasiyajatin68@gmail.com>
2024-07-11 15:42:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,188 @@
+const tl = gsap.timeline()
+
+  tl.to("#box1",{
+    x:1500,
+    rotate:360,
+    duration:1.5,
+    delay:1
+
+  })
+  tl.to("#box2",{
+    x:1500,
+    duration:1.5,
+  })
+
+  tl.to("#box3",{
+    x:1500,
+    duration:1.5,
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// gsap.to("#box1",{
+//   x:1500,
+//   duration:2,
+//   delay:1,
+//   rotate:360,
+//   // backgroundColor:"blue",
+//   // borderRadius:"50%",
+//   // scale:0.5
+// });
+// gsap.to("#box2",{
+//   x:1500,
+//   duration:2,
+//   delay:3,
+//   rotate:360,
+//   // repeat:-1,
+//   // yoyo:true,
+//   backgroundColor:"blue",
+
+
+// })
+// gsap.to("#box3",{
+//   x:1500,
+//   duration:2,
+//   delay:5,
+//   rotate:360,
+//   scale:0.5,
+//   // repeat:-1,
+//   // yoyo:true,
+//   backgroundColor:"blue",
+
+
+// })
+
+// // gsap.to("#box2",{
+// //   x:500,
+// //   y:500,
+// //   duration:2,
+// //   delay:1
+// // });
+
+// gsap.from("h1",{
+//   opacity:0,
+//   color:"red",
+//   duration:2,
+//   delay:1,
+//   y:20,
+//   stagger:-1
+// })
+
+
